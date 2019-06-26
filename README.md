76265b49d07dcd284bb7fba6ac90cbac91bdc201
Melf Stöcken <me@braune-digital.com>
2019-06-26 18:16:57 +0200

    public_api update

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Style Import
+you have to import manually the /lib/
+
+
 # Layout
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.0.4.
--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -3,5 +3,14 @@
 export * from './bd.layout.module';
 
 // Components
+export {BdLayoutComponent} from './components/layout/bd-layout/bd-layout.component';
 
-export { BdLayoutComponent } from './components/layout/bd-layout/bd-layout.component';
+// Services
+export {AlertService} from './services/alert.service';
+export {AbstractAlertService} from './services/alert.abstract.service';
+export {AlertInterface} from './services/alert.abstract.service';
+
+
+// Utils
+export {Bem} from './utils/bem';
+export {Breakpoints} from './utils/breakpoints';
