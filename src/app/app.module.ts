6701b10bbd9b4e3a285735a144b7c3430751f9a4
Melf Stöcken <me@braune-digital.com>
2019-07-16 09:56:26 +0200

    fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {BdLayoutModule} from '../../lib/src/bd.layout.module';
+import {GravatarModule} from 'ngx-gravatar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BdLayoutModule,
+    GravatarModule
   ],
+  exports: [AppComponent],
   providers: [],
   bootstrap: [AppComponent]
 })
