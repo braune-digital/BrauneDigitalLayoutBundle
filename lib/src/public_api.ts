0548c0e064526a28f7ad0117e21f633df3967997
Melf Stöcken <me@braune-digital.com>
2019-08-12 11:11:27 +0200

    added all component exports to public_api

--- lib/src/public_api.ts
+++ lib/src/public_api.ts
@@ -5,6 +5,95 @@ export * from './bd.layout.module';
 // Components
 export {BdLayoutComponent} from './components/layout/bd-layout/bd-layout.component';
 
+export {BdLoadingComponent} from './components/utils/bd-loading/bd-loading.component';
+export {BdMessageComponent} from './components/utils/bd-message/bd-message.component';
+export {BdProgressComponent} from './components/utils/bd-progress/bd-progress.component';
+export {BdProgressBarComponent} from './components/utils/bd-progress-bar/bd-progress-bar.component';
+export {BdBadgeComponent} from './components/utils/bd-badge/bd-badge.component';
+
+export {BdLayoutComponent} from './components/layout/bd-layout/bd-layout.component';
+export {BdBrandComponent} from './components/layout/bd-brand/bd-brand.component';
+export {BdContentComponent} from './components/layout/bd-content/bd-content.component';
+export {BdColComponent} from './components/layout/bd-col/bd-col.component';
+export {BdGridComponent} from './components/layout/bd-grid/bd-grid.component';
+export {BdAlertComponent} from './components/layout/bd-alert/bd-alert.component';
+export {BdOverlayComponent} from './components/layout/bd-overlay/bd-overlay.component';
+export {ContentLockComponent} from './components/layout/content-lock/content-lock.component';
+export {ContentLockContentComponent} from './components/layout/content-lock-content/content-lock-content.component';
+export {ContentLockPreviewComponent} from './components/layout/content-lock-preview/content-lock-preview.component';
+
+export {BdButtonComponent} from './components/elements/bd-button/bd-button.component';
+export {BdLinkListComponent} from './components/elements/bd-link-list/bd-link-list.component';
+export {BdLinkListItemComponent} from './components/elements/bd-link-list-item/bd-link-list-item.component';
+export {BdAvatarComponent} from './components/elements/bd-avatar/bd-avatar.component';
+export {BdActionGroupComponent} from './components/elements/bd-action-group/bd-action-group.component';
+export {BdIconComponent} from './components/elements/bd-icon/bd-icon.component';
+export {BdImageComponent} from './components/elements/bd-image/bd-image.component';
+export {BdBookmarkComponent} from './components/elements/bd-bookmark/bd-bookmark.component';
+export {BdDividerComponent} from './components/elements/bd-divider/bd-divider.component';
+export {BdSerpComponent} from './components/elements/bd-serp/bd-serp.component';
+export {BdThumbnailListComponent} from './components/elements/bd-thumbnail-list/bd-thumbnail-list.component';
+export {BdThumbnailListItemComponent} from './components/elements/bd-thumbnail-list-item/bd-thumbnail-list-item.component';
+export {BdApprovalComponent} from './components/elements/bd-approval/bd-approval.component';
+export {BdHighlightComponent} from './components/elements/bd-highlight/bd-highlight.component';
+export {BdTagsComponent} from './components/elements/bd-tags/bd-tags.component';
+export {BdUserProfileComponent} from './components/elements/bd-user-profile/bd-user-profile.component';
+export {BdSocialLinksComponent} from './components/elements/bd-social-links/bd-social-links.component';
+export {BdSocialShareComponent} from './components/elements/bd-social-share/bd-social-share.component';
+export {BdMembersComponent} from './components/elements/bd-members/bd-members.component';
+export {BdContentModalComponent} from './components/content/bd-content-modal/bd-content-modal.component';
+export {BdInfoBoxComponent} from './components/elements/bd-info-box/bd-info-box.component';
+export {BdMediaComponent} from './components/elements/bd-media/bd-media.component';
+export {BdImagePlaceholderComponent} from './components/elements/bd-image-placeholder/bd-image-placeholder.component';
+export {BdTagComponent} from './components/elements/bd-tag/bd-tag.component';
+export {BdGalleryComponent} from './components/elements/bd-gallery/bd-gallery.component';
+export {BdExampleComponent} from './components/elements/bd-example/bd-example.component';
+export {BdTabHeadComponent} from './components/elements/bd-tab-head/bd-tab-head.component';
+
+export {BdSearchEntityComponent} from './components/form/search-entity/bd-search-entity.component';
+export {BdUploadComponent} from './components/form/bd-upload/bd-upload.component';
+export {BdFormErrorComponent} from './components/form/form-error/bd-form-error.component';
+
+export {BdSearchUserModalComponent} from './components/modals/bd-search-user-modal/bd-search-user-modal.component';
+export {BdSearchPageModalComponent} from './components/modals/bd-search-page-modal/bd-search-page-modal.component';
+
+export {BdMasonryDirective} from './directive/bd-masonry/bd-masonry.directive';
+export {BdMasonryItemDirective} from './directive/bd-masonry/bd-masonry-item.directive';
+export {SafePipe} from './pipes/safe.pipe';
+export {AlertService} from './services/alert.service';
+export {AngularStickyThingsModule} from '@w11k/angular-sticky-things';
+export {BdContentModalMainComponent} from './components/content/bd-content-modal-main/bd-content-modal-main.component';
+export {BdContentModalFooterComponent} from './components/content/bd-content-modal-footer/bd-content-modal-footer.component';
+export {BdHeaderComponent} from './components/layout/bd-header/bd-header.component';
+export {BdSidebarComponent} from './components/layout/bd-sidebar/bd-sidebar.component';
+export {BdSidebarPreComponent} from './components/layout/bd-sidebar/bd-sidebar-pre/bd-sidebar-pre.component';
+export {BdSidebarPostComponent} from './components/layout/bd-sidebar/bd-sidebar-post/bd-sidebar-post.component';
+export {BdSidebarMainComponent} from './components/layout/bd-sidebar/bd-sidebar-main/bd-sidebar-main.component';
+export {BdSidebarListComponent} from './components/layout/bd-sidebar/bd-sidebar-list/bd-sidebar-list.component';
+export {BdSidebarListItemComponent} from './components/layout/bd-sidebar/bd-sidebar-list-item/bd-sidebar-list-item.component';
+export {BdLayoutContentComponent} from './components/layout/bd-layout/bd-layout-content/bd-layout-content.component';
+export {BdLayoutSidebarComponent} from './components/layout/bd-layout/bd-layout-sidebar/bd-layout-sidebar.component';
+export {BdVersionComponent} from './components/elements/bd-version/bd-version.component';
+export {BdContentHeaderComponent} from './components/layout/bd-content/bd-content-header/bd-content-header.component';
+export {BdContentMainComponent} from './components/layout/bd-content/bd-content-main/bd-content-main.component';
+export {BdContentAsideComponent} from './components/layout/bd-content/bd-content-aside/bd-content-aside.component';
+export {BdHeaderMainComponent} from './components/layout/bd-header/bd-header-main/bd-header-main.component';
+export {BdHeaderAsideComponent} from './components/layout/bd-header/bd-header-aside/bd-header-aside.component';
+export {BdHeaderPostAsideComponent} from './components/layout/bd-header/bd-header-post-aside/bd-header-post-aside.component';
+export {BdHeaderPostMainComponent} from './components/layout/bd-header/bd-header-post-main/bd-header-post-main.component';
+export {BdContentCenteredComponent} from './components/layout/bd-content/bd-content-centered/bd-content-centered.component';
+export {BdFileComponent} from './components/elements/bd-file/bd-file.component';
+export {BdActionGroupItemComponent} from './components/elements/bd-action-group/bd-action-group-item/bd-action-group-item.component';
+export {BdActionBarComponent} from './components/elements/bd-action-bar/bd-action-bar.component';
+export {BdActionBarGroupComponent} from './components/elements/bd-action-bar/bd-action-bar-group/bd-action-bar-group.component';
+export {BdItemComponent} from './components/elements/bd-item/bd-item.component';
+export {BdProgressCircleComponent} from './components/utils/bd-progress-circle/bd-progress-circle.component';
+export {BdNavAsideComponent} from './components/layout/bd-nav/bd-nav-aside/bd-nav-aside.component';
+export {BdNavComponent} from './components/layout/bd-nav/bd-nav.component';
+export {BdNavMainComponent} from './components/layout/bd-nav/bd-nav-main/bd-nav-main.component';
+export {BdLayoutNavComponent} from './components/layout/bd-layout/bd-layout-nav/bd-layout-nav.component';
+
+
 // Services
 export {AlertService} from './services/alert.service';
 export {AbstractAlertService} from './services/alert.abstract.service';
@@ -14,3 +103,5 @@ export {AlertInterface} from './services/alert.abstract.service';
 // Utils
 export {Bem} from './utils/bem';
 export {Breakpoints} from './utils/breakpoints';
+
+
